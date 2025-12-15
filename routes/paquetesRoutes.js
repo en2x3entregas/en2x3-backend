101f52bd1f256d81ee1db5cae0b89e0f7fdc63ee
en2x3entregas <en2x3entregas@gmail.com>
2025-12-15 15:24:02 -0500

    Remove comments from paquetesRoutes.js

--- routes/paquetesRoutes.js
+++ routes/paquetesRoutes.js
@@ -12,18 +12,16 @@ import {
 
 const router = Router();
 
-// Base: /api/paquetes
 router.get("/", listarPaquetes);
 router.post("/", crearPaquete);
 
-// Extra
 router.post("/geocode-lote", geocodificarLote);
 router.patch("/:id/estado", actualizarEstado);
 router.put("/:id/coords", actualizarCoords);
 
-// CRUD por ID
 router.get("/:id", obtenerPaquete);
 router.put("/:id", actualizarPaquete);
 router.delete("/:id", eliminarPaquete);
 
 export default router;
+
